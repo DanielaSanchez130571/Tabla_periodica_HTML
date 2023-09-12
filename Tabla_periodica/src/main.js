56d44446e4584d072222af82b77cc8bf63e49c66
DanHT22 <tolumes.daniela@gmail.com>
2023-09-12 17:51:56 -0600

    quite boostrap porque causaba conflicto

--- Tabla_periodica/src/main.js
+++ Tabla_periodica/src/main.js
@@ -1,7 +1,7 @@
 
 // import './assets/main.css'
 import './assets/CSS/style.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
